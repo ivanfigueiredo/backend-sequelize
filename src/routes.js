eb6b8cce9b5e1f5f4e66a3445e522e6dfb9f4c46
Ivan Figueiredo <ivan.figueiredo99@gmail.com>
2021-10-15 18:12:27 -0300

    Controller Course, Class e Grade

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const AuthController = require('./Controller/AuthController');
 const UserController = require('./Controller/UserController');
 const CourseController = require('./Controller/CourseController');
+const ClassController = require('./Controller/ClassController');
+const GradeController = require('./Controller/GradesController');
 
 const CourseValidator = require('./Validators/CourseValidator');
 const AuthValidator = require('./Validators/AuthValidator');
@@ -21,13 +23,24 @@ router.put('/user/update', UserValidator.updateUser, Auth.private, UserControlle
 //Listagem de Usuários
 router.get('/user/listusers', UserController.getUsers);
 //Informações de Usuário
-router.get('/user/info', UserController.infoUsers);
+router.get('/user/info', Auth.private, UserController.infoUsers);
 
 //Rota de cadastrado de Curso
 router.post('/course/add', CourseValidator.addCourse, Auth.private, CourseController.addCoursers);
 
 //Listar cursos
 router.get('/course/list', CourseController.getCoursers);
+
+//Criar turma
+router.post('/class/add', Auth.private, ClassController.create);
+//Listar todas as turmas
+router.get('/class/list', ClassController.getClass);
+
+//Lançar notas
+router.post('/grade/add', Auth.private, GradeController.create);
+
+//Listar todas as notas.
+router.get('/grades/list', GradeController.getGrades); 
 
 router.get('/me',(req, res) => {
     res.render('/me');
